4f796329cceeb6bf45cf8e2c309fbbce492b93c0
ParthNautiyaldev <parthnautiyal.dev@gmail.com>
2026-09-01 10:53:25 +0530

    a section is done

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const { sayhi } = require("F:/Career/Backend/node and express tute/5utils.js");
 const names = require("F:/Career/Backend/node and express tute/4names.js");
+const data = require("F:/Career/Backend/node and express tute/6alternativeflavour.js");
 
+console.log(data);
 sayhi("Susan");
 sayhi(names.john);
 sayhi(names.peter);
